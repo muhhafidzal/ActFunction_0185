2ad750038533a05908b73499eb04833339d7d68c
muhhafidzal <muh.hafidz.al.mubaroq@gmai.com>
2026-03-02 16:06:42 +0700

    membenarkan luas persegi (fungsi dengan parameter)

--- Luas_Persegi.cpp
+++ Luas_Persegi.cpp
@@ -4,3 +4,19 @@ using namespace std;
 //deklarasi variabel global
 int panjang, lebar;
 //implementasi fungsi dan prosedur
+void input(){
+    cout <<"Masukkan Panjang :";
+    cin >> panjang;
+    cout <<"Masukka Lebar ;";
+    cin >> lebar;
+}
+int luaspersegi(){
+    return panjang*lebar;
+}
+void output (){
+    cout<<"Hasilnya :" << luaspersegi();
+}
+int main (){ // main
+    input();
+    output();
+}//selesai
